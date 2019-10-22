a0ee602d8a0908f0df429db6ce545ba6358378ab
vaerizk <ezerblaze@gmail.com>
2019-10-22 18:20:18 +0300

    [Conan] Update build requirements and requirements

--- conanfile.py
+++ conanfile.py
@@ -27,11 +27,11 @@ class QuaZipConan(ConanFile):
     no_copy_source = True
 
     def build_requirements(self):
-        self.build_requires("cmake_installer/3.15.3@conan/stable")
+        self.build_requires("cmake_installer/[>3.0.0]@conan/stable")
 
     def requirements(self):
         self.requires("qt/5.13.1@bincrafters/stable", private=False)
-        self.requires("zlib/1.2.11@conan/stable", private=False)
+        self.requires("zlib/1.2.11", private=False)
 
     def build(self):
         cmake = CMake(self)
