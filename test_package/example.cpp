bdc9347e6d017129b72499a2a78315f2f37cd5ea
vaerizk <ezerblaze@gmail.com>
2019-11-19 19:50:50 +0300

    [Conan] Refactor test package

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -3,21 +3,21 @@
 #include <QFileInfo>
 #include <quazip.h>
 
-int main() {
+void main() {
 	QuaZip quazip;
 	quazip.setZipName("quazip_test_package.zip");
 	quazip.open(QuaZip::mdCreate);
 	quazip.close();
 	if (!QFileInfo(quazip.getZipName()).exists()) {
 		std::cout << "Something went wrong: test archive has not been created\n";
+		return;
+	}
+
+	std::cout << quazip.getZipName().toStdString() << " successfully created\n";
+	if (QDir().remove(quazip.getZipName())) {
+		std::cout << quazip.getZipName().toStdString() << " successfully removed\n";
 	}
 	else {
-		std::cout << quazip.getZipName().toStdString() << " successfully created\n";
-		if (QDir().remove(quazip.getZipName())) {
-			std::cout << quazip.getZipName().toStdString() << " successfully removed\n";
-		}
-		else {
-			std::cout << "Something went wrong:" << quazip.getZipName().toStdString() << " has not been removed\n";
-		}
+		std::cout << "Something went wrong:" << quazip.getZipName().toStdString() << " has not been removed\n";
 	}
 }
